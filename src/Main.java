0ea2bf81a6c9c8901decccb589ec8c3df66adbbd
officialdelffin <workdelffin@gmail.com>
2026-02-28 15:01:32 -0300

    commit 2

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
+// Projeto : Sitema Busca Filme em java :
+
+/* Aplicação Java desenvolvida para consolidar conhecimentos em consumo de APIs REST. O sistema realiza requisições HTTP para busca de filmes e exibe os dados processados diretamente no terminal.
+Explora o uso de HttpClient, HttpRequest e HttpResponse, focando na integração entre sistemas e manipulação de respostas externas. */
 
 
-
+// Classe princial do sismtea :
 public class Main {
 
 
+    // Metodo de execução pricipal do sistema :
     public static void main(String[] args) {
 
 

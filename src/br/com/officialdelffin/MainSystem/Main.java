cd4f71b8e90290c6c0d97f613698d484d39c7322
officialdelffin <workdelffin@gmail.com>
2026-03-02 20:32:26 -0300

    commit 15

--- src/br/com/officialdelffin/MainSystem/Main.java
+++ src/br/com/officialdelffin/MainSystem/Main.java
@@ -1,7 +1,7 @@
 
 /* Projeto : Sitema Busca Filme em java :
 
- Aplicação Java desenvolvida para consolidar conhecimentos em consumo de APIs REST. O sistema realiza requisições HTTP para busca de filmes e exibe os dados processados diretamente no terminal.
+Aplicação Java desenvolvida para consolidar conhecimentos em consumo de APIs REST. O sistema realiza requisições HTTP para busca de filmes e exibe os dados processados diretamente no terminal.
 Explora o uso de HttpClient, HttpRequest e HttpResponse, focando na integração entre sistemas e manipulação de respostas externas. */
 
 
---
+++ src/br/com/officialdelffin/Movies/Movies.java
@@ -0,0 +1,17 @@
+
+// Pacotes :
+package br.com.officialdelffin.Movies;
+
+
+// Classe resposável por criar objetos de filmes :
+public class Movies {
+
+
+    // Atributos :
+
+
+
+    // Métodos :
+
+
+}

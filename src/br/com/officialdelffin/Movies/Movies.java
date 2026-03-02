5e6ad87d290ab1b31dc43c36d10d235456646bdc
officialdelffin <workdelffin@gmail.com>
2026-03-02 20:38:12 -0300

    commit 16

--- src/br/com/officialdelffin/Movies/Movies.java
+++ src/br/com/officialdelffin/Movies/Movies.java
@@ -9,6 +9,14 @@ public class Movies {
 
     // Atributos :
 
+    private String title;
+    private String year;
+    private String released;
+    private String runtime;
+    private String genre;
+    private String director;
+    private String writer;
+    private String plot;
 
 
     // Métodos :
